57f48d4a70884c0ebb12e8ef982d4b7aab38f1e3
booya-tech <booya.pannachai@gmail.com>
2024-10-29 13:05:58 +0700

    first commit: push a repository to git

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by Panachai Sulsaksakul on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatAppDemoTests/ChatAppDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoTests.swift
+//  ChatAppDemoTests
+//
+//  Created by Panachai Sulsaksakul on 10/29/24.
+//
+
+import Testing
+@testable import ChatAppDemo
+
+struct ChatAppDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
